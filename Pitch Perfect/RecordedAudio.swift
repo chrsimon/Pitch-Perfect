5a0d25c48d5f5cc8771fccb9c295d20467e006b5
Chris Simon <chrsimon@umich.edu>
2015-01-19 13:39:27 -0500

    Done!
    Completed Udacity Mobile App Development Course

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Christopher Simon on 1/15/15.
+//  Copyright (c) 2015 Christopher Simon. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
